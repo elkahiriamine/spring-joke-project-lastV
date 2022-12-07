ce460468e39f990468567c5c4e57e67cf3ec25b2
AmineELKAHIRI <elkahiriamine1999@gmail.com>
2022-12-07 20:25:18 +0100

    some modification on spring configuration

---
+++ src/test/java/com/example/springjokeproject/SpringJokeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjokeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
